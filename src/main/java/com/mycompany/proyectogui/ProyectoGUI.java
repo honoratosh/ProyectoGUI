2e3bf18100079decf7921fda5a6c4471c08d9e2d
Honorato Saavedra Hernández <honoratosaavedra@gmail.com>
2024-05-16 07:43:58 -0600

    Inicio

---
+++ src/main/java/com/mycompany/proyectogui/ProyectoGUI.java
@@ -0,0 +1,15 @@
+
+package com.mycompany.proyectogui;
+
+public class ProyectoGUI {
+
+    public static void main(String[] args) {
+        //Crear una ventana con un botón con su número de cuenta
+        //El botón debe abrir otra ventana donde se muestran
+        //su nombre, número de cuenta, fecha de nacimiento,
+        //dirección y fotografía.
+        
+        //Subir el proyecto completo de netbeans a github
+        //Enviar la liga del proyecto al servidor de discord
+    }
+}
